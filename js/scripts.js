fc1903a22ca17d0ead502a597be74ac7977775b4
JasonShadle <jason.shadle50@gmail.com>
2019-04-29 13:56:31 -0700

    Repo rehaul

--- js/scripts.js
+++ js/scripts.js
@@ -95,14 +95,3 @@
     });
 
 })(jQuery);
-
-function getAge() {
-    var today = new Date();
-    var birthDate = new Date("1997/08/27");
-    var age = today.getFullYear() - birthDate.getFullYear();
-    var m = today.getMonth() - birthDate.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
-    }
-    return age;
-};
--- younglife/README.md
+++
@@ -1,3 +0,0 @@
-# Younglife
-
-This was a site that I was in the process of making for a friend for a club at CSU. Ultimately she decided that she didn't want it due to club rules, so I stopped my progress. If I have time later on I might finish it just as an example. I used a Bootstrap template and edited it as I went along.
