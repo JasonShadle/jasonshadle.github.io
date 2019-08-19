6eb35eb4907e9c1187c35f7e4d226931f38ca623
JasonShadle <jason.shadle50@gmail.com>
2019-08-19 15:59:02 -0400

    Start of xray lines, audit run time

--- visionair/gulpfile.js
+++ visionair/gulpfile.js
@@ -4,7 +4,7 @@ var exec = require('child_process').exec;
 
 
 gulp.task('less', function() {
-    exec('lessc --strict-math=on ./css/custom.less ./style.css');
+    exec('lessc --math=strict ./css/custom.less ./style.css');
 });
 gulp.task('watch', function() {
     gulp.watch('./css/*.less', ['less']);  // Watch all the .less files, then run the less task
